7e364c630a614b4b40755eb91cf67bfd62c1083a
Derick Yang <derick.yang@tufts.edu>
2016-03-08 00:05:24 -0500

    started messages

---
+++ messages/README.md
@@ -0,0 +1,3 @@
+README
+Messages Lab
+By: Derick Yang
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,23 @@
 // Your JavaScript goes here...
+function parse() {
+    var request = new XMLHttpRequest();
+    var messages = document.getElementById("messages");
+    var data;
+    request.open("GET", "data.json", true);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            data = JSON.parse(request.responseText);
+            for (var i = 0; i < data.length; i++) {
+                var newmessage = document.createElement("p");
+                var sender = document.createElement("p");
+                newmessage.setAttribute("class", "msg");
+                sender.setAttribute("class", "sender");
+                newmessage.innerHTML = data[i].content;
+                sender.innerHTML = data[i].username;
+                messages.appendChild(newmessage);
+                messages.appendChild(sender);
+            }
+        }
+    };
+    request.send(null);
+}
